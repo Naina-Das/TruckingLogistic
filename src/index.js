f3ffd3182ff06bad2a718192db66b2e10975fa31
Naina <naina.das3698@gmail.com>
2021-08-02 00:41:33 +0530

    Token Added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("../public/firebase-messaging-sw.js")
+    .register("./firebase-messaging-sw.js")
     .then(function(registration) {
       console.log("Registration successful, scope is:", registration.scope);
     })
